6dcda6d980418da8e6c93e988fb4aa73c2e0dd1b
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-06-15 14:34:37 +0200

    Use DialogFragment instead of fragment

--- app/src/main/java/pl/jadebusem/gpscoordinatescollector/AllPlacesList.java
+++ app/src/main/java/pl/jadebusem/gpscoordinatescollector/AllPlacesList.java
@@ -1,14 +1,12 @@
 package pl.jadebusem.gpscoordinatescollector;
 
-import android.app.Fragment;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -83,14 +81,18 @@ public class AllPlacesList extends AppCompatActivity {
 				Bundle placeToShow = new Bundle();
 				placeToShow.putString("PLACE_NAME", place_name);
 
-				Fragment fragment = new PlaceDetailsFragment();
+				DialogFragment fragment = new PlaceDetailsFragment();
 				fragment.setArguments(placeToShow);
-
-				getFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+				FragmentManager fragmentManager = getFragmentManager();
+				fragment.show(fragmentManager,"fsdf");
 
 			}
 		});
 	}
+
+
+
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/pl/jadebusem/gpscoordinatescollector/PlaceDetailsFragment.java
+++ app/src/main/java/pl/jadebusem/gpscoordinatescollector/PlaceDetailsFragment.java
@@ -1,6 +1,6 @@
 package pl.jadebusem.gpscoordinatescollector;
 
-import android.app.Fragment;
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,13 +9,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class PlaceDetailsFragment extends Fragment {
+public class PlaceDetailsFragment extends DialogFragment {
+
+	public static final String USTAWIANIE_POZYCJI = "Ustawianie pozycji";
 
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.item_details_fragment, container, false);
-
+		getDialog().setTitle(USTAWIANIE_POZYCJI);
 		String placeName = getArguments().getString("PLACE_NAME");
 		TextView textView = (TextView)view.findViewById(R.id.itemName);
 		textView.setText(placeName);
@@ -23,7 +25,7 @@ public class PlaceDetailsFragment extends Fragment {
 		closeButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				getActivity().getFragmentManager().beginTransaction().hide(PlaceDetailsFragment.this).commit();
+				dismiss();
 			}
 		});
 
@@ -31,7 +33,7 @@ public class PlaceDetailsFragment extends Fragment {
 		setButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				getActivity().getFragmentManager().beginTransaction().hide(PlaceDetailsFragment.this).commit();
+				dismiss();
 			}
 		});
 
