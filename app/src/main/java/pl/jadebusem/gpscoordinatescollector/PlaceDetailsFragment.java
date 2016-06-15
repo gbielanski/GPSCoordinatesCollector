e64435838481b67edec6bd8668472ec959388487
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-06-15 15:08:00 +0200

    Location manager and location listener added

--- app/src/main/java/pl/jadebusem/gpscoordinatescollector/PlaceDetailsFragment.java
+++ app/src/main/java/pl/jadebusem/gpscoordinatescollector/PlaceDetailsFragment.java
@@ -1,17 +1,26 @@
 package pl.jadebusem.gpscoordinatescollector;
 
+import android.Manifest;
 import android.app.DialogFragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PlaceDetailsFragment extends DialogFragment {
 
 	public static final String USTAWIANIE_POZYCJI = "Ustawianie pozycji";
+	private Button setButton;
 
 	@Nullable
 	@Override
@@ -19,9 +28,9 @@ public class PlaceDetailsFragment extends DialogFragment {
 		View view = inflater.inflate(R.layout.item_details_fragment, container, false);
 		getDialog().setTitle(USTAWIANIE_POZYCJI);
 		String placeName = getArguments().getString("PLACE_NAME");
-		TextView textView = (TextView)view.findViewById(R.id.itemName);
+		TextView textView = (TextView) view.findViewById(R.id.itemName);
 		textView.setText(placeName);
-		Button closeButton = (Button)view.findViewById(R.id.close_button);
+		Button closeButton = (Button) view.findViewById(R.id.close_button);
 		closeButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -29,13 +38,51 @@ public class PlaceDetailsFragment extends DialogFragment {
 			}
 		});
 
-		Button setButton = (Button)view.findViewById(R.id.set_button);
+		setButton = (Button) view.findViewById(R.id.set_button);
 		setButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				dismiss();
 			}
 		});
+
+		LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+		LocationListener locationListener = new LocationListener() {
+			@Override
+			public void onLocationChanged(Location location) {
+				Toast.makeText(getActivity(), "Location: " + location.toString(), Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+
+			}
+
+			@Override
+			public void onProviderEnabled(String provider) {
+
+			}
+
+			@Override
+			public void onProviderDisabled(String provider) {
+
+			}
+		};
+
+
+		if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+				ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			// TODO: Consider calling
+			//    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for Activity#requestPermissions for more details.
+			return view;
+		}
+
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
 		return view;
 	}
