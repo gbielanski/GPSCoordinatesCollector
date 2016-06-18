96042e2b0d394b9eaf731cf8ce09a2e90c78f416
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-06-18 20:49:56 +0200

    isBetterLocation copied from Android Guide

--- app/src/main/java/pl/jadebusem/gpscoordinatescollector/PlaceDetailsFragment.java
+++ app/src/main/java/pl/jadebusem/gpscoordinatescollector/PlaceDetailsFragment.java
@@ -36,6 +36,7 @@ public class PlaceDetailsFragment extends DialogFragment {
 	private Firebase mRootref;
 	private Place place;
 	private int position;
+	private Location curentLocation;
 
 
 	@Nullable
@@ -91,8 +92,11 @@ public class PlaceDetailsFragment extends DialogFragment {
 			@Override
 			public void onLocationChanged(Location location) {
 
-				lon.setText(getFormatedLocation(location.getLongitude()));
-				lat.setText(getFormatedLocation(location.getLatitude()));
+				if(isBetterLocation(location, curentLocation)) {
+					lon.setText(getFormatedLocation(location.getLongitude()));
+					lat.setText(getFormatedLocation(location.getLatitude()));
+					curentLocation = location;
+				}
 			}
 
 			@Override
@@ -121,11 +125,9 @@ public class PlaceDetailsFragment extends DialogFragment {
 	@Override
 	public void onResume() {
 		super.onResume();
-		if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-				ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+		if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 			ActivityCompat.requestPermissions(getActivity(),
 					new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_GPS_REQUEST);
-			Toast.makeText(getActivity(), "NO GPS PERMISSION", Toast.LENGTH_LONG).show();
 			return ;
 		}
 
@@ -136,11 +138,68 @@ public class PlaceDetailsFragment extends DialogFragment {
 	@Override
 	public void onPause() {
 		super.onPause();
-		if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-				ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+		if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			ActivityCompat.requestPermissions(getActivity(),
+					new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_GPS_REQUEST);
 			return ;
 		}
 
 		locationManager.removeUpdates(locationListener);
+	}
+
+	private static final int TWO_MINUTES = 1000 * 60 * 2;
+
+	/** Determines whether one Location reading is better than the current Location fix
+	 * @param location  The new Location that you want to evaluate
+	 * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+	 */
+	protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+		if (currentBestLocation == null) {
+			// A new location is always better than no location
+			return true;
+		}
+
+		// Check whether the new location fix is newer or older
+		long timeDelta = location.getTime() - currentBestLocation.getTime();
+		boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+		boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+		boolean isNewer = timeDelta > 0;
+
+		// If it's been more than two minutes since the current location, use the new location
+		// because the user has likely moved
+		if (isSignificantlyNewer) {
+			return true;
+			// If the new location is more than two minutes older, it must be worse
+		} else if (isSignificantlyOlder) {
+			return false;
+		}
+
+		// Check whether the new location fix is more or less accurate
+		int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+		boolean isLessAccurate = accuracyDelta > 0;
+		boolean isMoreAccurate = accuracyDelta < 0;
+		boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+		// Check if the old and new location are from the same provider
+		boolean isFromSameProvider = isSameProvider(location.getProvider(),
+				currentBestLocation.getProvider());
+
+		// Determine location quality using a combination of timeliness and accuracy
+		if (isMoreAccurate) {
+			return true;
+		} else if (isNewer && !isLessAccurate) {
+			return true;
+		} else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+			return true;
+		}
+		return false;
+	}
+
+	/** Checks whether two providers are the same */
+	private boolean isSameProvider(String provider1, String provider2) {
+		if (provider1 == null) {
+			return provider2 == null;
+		}
+		return provider1.equals(provider2);
 	}
 }
